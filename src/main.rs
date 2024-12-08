fcf04f6ceb1babcfcdbd99378eb63093f0dd668b
Samurai_Chia <jyjzone@163.com>
2024-12-08 15:07:44 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+
+    change(&s);
+}
+
+fn change(some_string: &String) {
+    //下面的語句語法通過編譯，因為some_string是一個String的引用只是借用了s的值沒有取得所有權，所以無法對原值進行修改
+    //some_string.push_str(", world");
 }
